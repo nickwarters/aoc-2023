01d36abae944f0cedf191009401583e42509ee8d
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2023-12-01 06:44:03 +0000

    day 1 - ts

--- js/day01/main.ts
+++ js/day01/main.ts
@@ -1,16 +1,44 @@
-function solve_day_one(input: string): number {
-	console.log(input)
-	return 0
+const fs = require('fs')
+
+function solve_day_one(input: string, replace: boolean = false): number {
+    let total = 0
+    const subs = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for (let line of input.split('\n')) {
+        if (!line) {
+            continue
+        }
+
+        const linePre = line
+
+        if (replace) {
+            for (let i = 0; i < subs.length; i++) {
+                console.log(`attemping to replace ${subs[i]} with ${subs[i].substring(0, 1) + String(i + 1) + subs[i].substring(subs[i].length - 1)}`)
+                line = line.replaceAll(subs[i], subs[i].substring(0, 1) + String(i + 1) + subs[i].substring(subs[i].length - 1))
+            }
+        }
+
+        const digits = Array.from(line.matchAll(/\d/g))
+
+        console.log(`linePre=${linePre}, line='${line}', digits=${digits}`)
+
+        total += Number(String(digits[0]) + String(digits[digits.length - 1]))
+    }
+
+    return total
 }
 
 function solve_day_two(input: string): number {
-	console.log(input)
-	return 0
+    return solve_day_one(input, true)
 }
 
-process.stdin.on('data', (input) => {
-	const part_one_result = solve_day_one(String(input))
-	const part_two_result = solve_day_two(String(input))
-	console.log(`part one: ${part_one_result}`)
-	console.log(`part two: ${part_two_result}`)
-})
+function main() {
+    const data = fs.readFileSync(0, 'utf-8')
+    console.log(data)
+    const part_one_result = solve_day_one(String(data))
+    const part_two_result = solve_day_two(String(data))
+    console.log(`part one: ${part_one_result}`)
+    console.log(`part two: ${part_two_result}`)
+}
+
+main()
