8f6da1518947503e5c60e6fb354662a8cd50586c
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2023-12-01 06:07:17 +0000

    add day 1 - python

--- python/day01/main.py
+++ python/day01/main.py
@@ -1,13 +1,54 @@
+import re
+
 def main():
     input_text = open(0).read()
     print(f'part one: {solve_part_one(input_text)}')
     print(f'part two: {solve_part_two(input_text)}')
 
 def solve_part_one(input_text: str):
-    pass
+    
+    total = 0
+
+    try:
+    
+        for i, line in enumerate(input_text.split('\n')):
+            if line == '':
+                continue
+            
+            digits = re.findall('\d', line)
+            total += int(digits[0] + digits[-1])
+    finally:
+
+        return total
+
 
 def solve_part_two(input_text: str):
-    pass
+    total = 0
+
+    number_map = (
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+    )
+    
+    for i, line in enumerate(input_text.split('\n')):
+        if line == '':
+            continue
+        
+        for i, s in enumerate(number_map):
+            line = line.replace(s, s[0] + str(i +1) + s[-1])
+        
+        digits = re.findall('\d', line)
+
+        total += int(digits[0] + digits[-1])
+
+    return total
 
 if __name__ == '__main__':
     raise SystemExit(main())
