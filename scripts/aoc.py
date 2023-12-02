a8f96cb0c04bc168f4c2be81f0a36caf237462e2
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2023-12-02 10:58:24 +0000

    add script for running code

--- scripts/aoc.py
+++ scripts/aoc.py
@@ -2,7 +2,7 @@ import argparse
 import datetime
 import os
 import subprocess
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 TODAY = datetime.datetime.today()
 SCRIPT_PATH = os.path.dirname(__file__)
@@ -15,27 +15,27 @@ VALID_LANGS = ('all', 'py', 'ts', 'go')
 
 def ts_run_cmd(day: int, input_path: str, year: int = 0, test: bool = False) -> List[str]:
     code_path = os.path.join(TS_DIR, f'day{str(day).zfill(2)}', 'main.ts')
-    return ['bun', 'run', code_path, '<', input_path]
+    return ['bun', 'run', code_path, ]
 
 
 def go_run_cmd(day: int, input_path: str, year: int = 0, test: bool = False) -> List[str]:
     code_path = os.path.join(GO_DIR, 'cmd', f'day{str(day).zfill(2)}', 'main.go')
     if input_path is None:
         input_path = get_input_path(day=day, test=test)
-    return ['go', 'run', code_path, '<', input_path]
+    return ['go', 'run', code_path,]
 
 
 def py_run_cmd(day: int, input_path: str, year: int = 0, test: bool = False) -> List[str]:
     code_path = os.path.join(PY_DIR, f'day{str(day).zfill(2)}', 'main.py')
     input_path = get_input_path(day=day, test=test)
-    return ['python', code_path, '<', input_path]
+    return ['python', code_path, ]
 
 
 def get_input_path(day: int, year: int = 0, test: bool = False) -> str:
-    return os.path.join(os.path.dirname(__file__).replace('scripts', 'inputs'), f'input{str(day).zfill(2)}{"_test" if test else ""}.txt')
+    return os.path.join(os.path.dirname(__file__).replace('scripts', 'inputs'), f'day{str(day).zfill(2)}{"_test" if test else ""}.txt')
 
 
-def get_all_commands(lang: str, version: str, day: int) -> List[List[str]]:
+def get_all_commands(lang: str, version: str, day: int) -> List[Tuple[List[str], str]]:
     commands = []
 
     if day == 0:
@@ -49,29 +49,36 @@ def get_all_commands(lang: str, version: str, day: int) -> List[List[str]]:
         langs = (lang,)
 
     if version == 'all':
-        tests = (False, True)
+        tests = (True, False)
     else:
         tests = (version == 'test',)
     
     for d in days:
         for l in langs: 
-            title = '=' * int((80 - len(l) - 4) / 2) + f'  {l}  ' + '=' * int((80 - len(l) - 4) / 2)
+            day_lang = f'Day: {d} - {l}'
+            title = '=' * int((80 - len(day_lang) - 4) / 2) + f'  {day_lang}  ' + '=' * int((80 - len(day_lang) - 4) / 2)
             match l:
                 case 'py':
                     for t in tests:
                         input_path = get_input_path(d, test=t)
-                        commands.append(['echo', title])
-                        commands.append(py_run_cmd(day=d, test=t, input_path=input_path))
+                        commands.append((['echo', title], ''))
+                        if t:
+                            commands.append((['echo', 'TEST'], ''))
+                        commands.append((py_run_cmd(day=d, test=t, input_path=input_path), input_path))
                 case 'ts':
                     for t in tests:
                         input_path = get_input_path(d, test=t)
-                        commands.append(['echo', title])
-                        commands.append(ts_run_cmd(day=d, test=t, input_path=input_path))
+                        commands.append((['echo', title], ''))
+                        if t:
+                            commands.append((['echo', 'TEST'], ''))
+                        commands.append((ts_run_cmd(day=d, test=t, input_path=input_path), input_path))
                 case 'go':
                     for t in tests:
                         input_path = get_input_path(d, test=t)
-                        commands.append(['echo', title])
-                        commands.append(go_run_cmd(day=d, test=t, input_path=input_path))
+                        commands.append((['echo', title], ''))
+                        if t:
+                            commands.append((['echo', 'TEST'], ''))
+                        commands.append((go_run_cmd(day=d, test=t, input_path=input_path), input_path))
                 case _:
                     continue
     return commands
@@ -82,7 +89,7 @@ def main() -> None:
     ap = argparse.ArgumentParser()
     ap.add_argument('--lang', action='store', default='all')
     ap.add_argument('--year', action='store', type=int, default=TODAY.year)
-    ap.add_argument('--day', action='store', type=int, default=0)
+    ap.add_argument('--day', action='store', type=int, default=TODAY.day)
     ap.add_argument('--version', action='store', default='all')
     ap.add_argument('--input', action='store', required=False)
     args = ap.parse_args()
@@ -91,8 +98,13 @@ def main() -> None:
         raise ValueError(f'invalid language! expected {VALID_LANGS}, got="{args.lang}"')
     
     all_commands = get_all_commands(args.lang, args.version, args.day)
-    for command in all_commands:
-        subprocess.run(command)
+    for command, input_path in all_commands:
+        if input_path == '':
+
+            subprocess.run(command)
+        else:
+            with open(input_path) as input_file:
+                subprocess.run(command, stdin=input_file)
 
 
 
