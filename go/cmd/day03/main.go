b60e269acfccadd49247e0cbba5addc7b986958c
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2023-12-03 07:28:36 +0000

    add day 3 go files
    go and ts progress for day 3
    
    remove huge file

--- go/cmd/day03/main.go
+++ go/cmd/day03/main.go
@@ -4,10 +4,100 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type N struct {
+	value int
+	row int
+	startCol int
+	endCol int
+}
+
+type S struct {
+	value string
+	row int
+	col int
+}
 
 func solve_part_one(input_text string) int {
 	total := 0
+
+	grid := strings.Split(input_text, "\n")
+
+	numbers := []N{}
+	symbols := []S{}
+
+	for r, row := range grid{
+		inNum := false
+		num := ""
+		numstart := 0
+		for c, ch := range row{
+			_, err := strconv.Atoi(string(ch))
+			if err != nil {
+				if inNum {
+					n, _ := strconv.Atoi(num)
+					numbers = append(numbers, N{
+						value: n,
+						startCol: numstart,
+						endCol: c - 1,
+						row: r,
+					})
+					inNum = false
+					numstart = 0
+					num = ""
+				}
+
+				if string(ch) != "." {
+					symbols = append(symbols, S{
+						row: r,
+						col: c,
+						value: string(ch),
+					})
+				}
+
+			} else {
+				if !inNum {
+					numstart = c
+				}
+				inNum = true
+				num += string(ch)
+			}
+		}
+	}
+
+	// directions := [][]int{
+	// 	[]int{-1, -1},
+	// 	[]int{-1, 0},
+	// 	[]int{-1, 1},
+	// 	[]int{0, -1},
+	// 	[]int{0, 1},
+	// 	[]int{1, -1},
+	// 	[]int{1, -1},
+	// 	[]int{1, 1},
+	// }
+
+	for _, n := range numbers {
+		valid := false
+		for _, s := range symbols {
+			if s.row >= n.row -1 && s.row <= n.row + 1 && s.col >= n.startCol - 1 && s.col <= n.endCol + 1{
+				valid = true
+				// fmt.Printf("added --> num: %v, sym: %v\n", n, s)
+				break
+			}
+		}
+		if valid{
+			total += n.value
+		} else {
+			fmt.Printf("not added --> num: %v\n", n)
+		}
+	}
+
+
+
+	// fmt.Printf("NUMBERS\n%v\n\nSYMBOLS\n%v\n", numbers, symbols)
+
 
 	return total
 }
--- js/day03/main.ts
+++ js/day03/main.ts
@@ -1,27 +1,135 @@
 import fs from 'fs'
 
 type S = {
-    start: number
-    end: number
+	row: number
+	column: number
+	value: string
 }
 
 type N = {
-    value: number
-    start: number
-    end: number
+	value: number
+	row: number
+	start: number
+	end: number
+	isValid: boolean
 }
 
 function solve_day_one(input: string): number {
-    let total = 0
+	let total = 0
 
-    const grid = input.split('\n')
+	const grid = input.split('\n')
 
-    const symbols: Array<number>[] = []
-    const numbers: Array<number>[] = []
+	const symbols: S[] = []
+	const numbers: N[] = []
 
-    for (let r = 0; r < grid.length; r++) {}
+	const allNumbers = '1234567890'
 
-    return total
+	const directions = [
+		[-1, 0],
+		[-1, -1],
+		[-1, 1],
+		[0, -1],
+		[0, 1],
+		[1, -1],
+		[1, 0],
+		[1, 1],
+	]
+
+	total = input
+		.split('\n')
+		.reduce((numbers, row, r, rows) => {
+			let inNum = false
+			let num = ''
+			let numStart = 0
+			let isValid = false
+
+			row.split('').forEach((ch, c) => {
+				const isNumber = allNumbers.includes(ch)
+				if (isNumber) {
+					num += ch
+					if (!inNum) {
+						numStart = c
+					}
+					inNum = true
+				} else {
+					inNum = false
+				}
+
+				if (!isValid && (isNumber || num !== '')) {
+					let sr: number
+					let sc: number
+
+					for (const d of directions) {
+						sr = r + d[0]
+						sc = c + d[1]
+						if (sr < 0 || sr >= rows.length || sc < 0 || sc >= row.length) {
+							continue
+						}
+
+						if (rows[sr][sc] !== '.' && !allNumbers.includes(rows[sr][sc])) {
+							isValid = true
+							// console.log({ r, c, ch, sr, sc, sym: rows[sr][sc] })
+							break
+						}
+					}
+				}
+
+				if (!isNumber && num != '') {
+					numbers.push({
+						value: Number(num),
+						row: r,
+						start: numStart,
+						end: c - 1,
+						isValid,
+					})
+
+					num = ''
+					numStart = 0
+					isValid = false
+				}
+			})
+
+			return numbers
+		}, numbers)
+		.filter((n) => {
+			// console.log(n)
+			return n.isValid
+		})
+		.reduce((sum, n) => sum + n.value, 0)
+
+	// console.log(
+	// 	numbers.filter(
+	// 		(n) =>
+	// 			symbols.filter(
+	// 				(s) =>
+	// 					!(
+	// 						s.row >= n.row - 1 &&
+	// 						s.row <= n.row + 1 &&
+	// 						s.column >= n.start - 1 &&
+	// 						s.column <= n.end + 1
+	// 					)
+	// 			).length > 0
+	// 	)
+	// )
+
+	// total = numbers
+	// 	.filter(
+	// 		(n) =>
+	// 			symbols.filter((s) => {
+	// 				return (
+	// 					s.row >= n.row - 1 &&
+	// 					s.row <= n.row + 1 &&
+	// 					s.column >= n.start - 1 &&
+	// 					s.column <= n.end + 1
+	// 				)
+	// 			}).length > 0
+	// 	)
+	// 	.reduce((t, n) => {
+	// 		console.log(n)
+	// 		return t + n.value
+	// 	}, 0)
+
+	return total
 }
 
 function solve_day_two(input: string): number {
