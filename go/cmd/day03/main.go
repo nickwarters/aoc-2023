769cd55e4f8296f2069d0f690570178239f70479
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2023-12-03 07:28:36 +0000

    add day 3 go files

---
+++ go/cmd/day03/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func solve_part_one(input_text string) int {
+	total := 0
+
+	return total
+}
+
+func solve_part_two(input_text string) int {
+	total := 0
+
+	return total
+}
+
+func main() {
+	stdin, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(stdin)
+
+	part_one_result := solve_part_one(input)
+	part_two_result := solve_part_two(input)
+	fmt.Printf("part one: %d\n", part_one_result)
+	fmt.Printf("part two: %d\n", part_two_result)
+}
