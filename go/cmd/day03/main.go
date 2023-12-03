6537e20fea669c0061b8d25ab41fdb0b716a4e39
Nick Warters <51464280+nickwarters@users.noreply.github.com>
2023-12-03 19:04:19 +0000

    day 3 - go

--- go/cmd/day03/main.go
+++ go/cmd/day03/main.go
@@ -9,16 +9,16 @@ import (
 )
 
 type N struct {
-	value int
-	row int
+	value    int
+	row      int
 	startCol int
-	endCol int
+	endCol   int
 }
 
 type S struct {
 	value string
-	row int
-	col int
+	row   int
+	col   int
 }
 
 func solve_part_one(input_text string) int {
@@ -29,20 +29,20 @@ func solve_part_one(input_text string) int {
 	numbers := []N{}
 	symbols := []S{}
 
-	for r, row := range grid{
+	for r, row := range grid {
 		inNum := false
 		num := ""
 		numstart := 0
-		for c, ch := range row{
+		for c, ch := range row {
 			_, err := strconv.Atoi(string(ch))
 			if err != nil {
 				if inNum {
 					n, _ := strconv.Atoi(num)
 					numbers = append(numbers, N{
-						value: n,
+						value:    n,
 						startCol: numstart,
-						endCol: c - 1,
-						row: r,
+						endCol:   c - 1,
+						row:      r,
 					})
 					inNum = false
 					numstart = 0
@@ -51,8 +51,8 @@ func solve_part_one(input_text string) int {
 
 				if string(ch) != "." {
 					symbols = append(symbols, S{
-						row: r,
-						col: c,
+						row:   r,
+						col:   c,
 						value: string(ch),
 					})
 				}
@@ -64,46 +64,103 @@ func solve_part_one(input_text string) int {
 				inNum = true
 				num += string(ch)
 			}
+			if c == len(row)-1 && inNum {
+				n, _ := strconv.Atoi(num)
+				numbers = append(numbers, N{
+					value:    n,
+					startCol: numstart,
+					endCol:   c - 1,
+					row:      r,
+				})
+
+			}
 		}
 	}
-
-	// directions := [][]int{
-	// 	[]int{-1, -1},
-	// 	[]int{-1, 0},
-	// 	[]int{-1, 1},
-	// 	[]int{0, -1},
-	// 	[]int{0, 1},
-	// 	[]int{1, -1},
-	// 	[]int{1, -1},
-	// 	[]int{1, 1},
-	// }
 
 	for _, n := range numbers {
 		valid := false
 		for _, s := range symbols {
-			if s.row >= n.row -1 && s.row <= n.row + 1 && s.col >= n.startCol - 1 && s.col <= n.endCol + 1{
+			if s.row >= n.row-1 && s.row <= n.row+1 && s.col >= n.startCol-1 && s.col <= n.endCol+1 {
 				valid = true
-				// fmt.Printf("added --> num: %v, sym: %v\n", n, s)
 				break
 			}
 		}
-		if valid{
+		if valid {
 			total += n.value
-		} else {
-			fmt.Printf("not added --> num: %v\n", n)
 		}
 	}
-
-
-
-	// fmt.Printf("NUMBERS\n%v\n\nSYMBOLS\n%v\n", numbers, symbols)
-
 
 	return total
 }
 
 func solve_part_two(input_text string) int {
 	total := 0
+
+	grid := strings.Split(input_text, "\n")
+
+	numbers := []N{}
+	symbols := []S{}
+
+	for r, row := range grid {
+		inNum := false
+		num := ""
+		numstart := 0
+		for c, ch := range row {
+			_, err := strconv.Atoi(string(ch))
+			if err != nil {
+				if inNum {
+					n, _ := strconv.Atoi(num)
+					numbers = append(numbers, N{
+						value:    n,
+						startCol: numstart,
+						endCol:   c - 1,
+						row:      r,
+					})
+					inNum = false
+					numstart = 0
+					num = ""
+				}
+
+				if string(ch) == "*" {
+					symbols = append(symbols, S{
+						row:   r,
+						col:   c,
+						value: string(ch),
+					})
+				}
+
+			} else {
+				if !inNum {
+					numstart = c
+				}
+				inNum = true
+				num += string(ch)
+			}
+			if c == len(row)-1 && inNum {
+				n, _ := strconv.Atoi(num)
+				numbers = append(numbers, N{
+					value:    n,
+					startCol: numstart,
+					endCol:   c - 1,
+					row:      r,
+				})
+
+			}
+		}
+	}
+
+	for _, s := range symbols {
+		g_nums := []N{}
+
+		for _, n := range numbers {
+			if s.row >= n.row-1 && s.row <= n.row+1 && s.col >= n.startCol-1 && s.col <= n.endCol+1 {
+				g_nums = append(g_nums, n)
+			}
+		}
+		if len(g_nums) == 2 {
+			total += g_nums[0].value * g_nums[1].value
+		}
+	}
 
 	return total
 }
