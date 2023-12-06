d44dd55401bca8c81bc1ee1b642f7177d92d96c8
Nick Warters <nick.warters@gmail.com>
2023-12-06 00:25:17 +0000

    still nowhere near with python day 5 part 2

--- python/day05/main.py
+++ python/day05/main.py
@@ -1,4 +1,6 @@
-import re
+import math
+from collections import Counter
+from typing import List, Tuple
 
 
 def main():
@@ -36,35 +38,47 @@ def solve_part_one(input_text: str) -> int:
     return total
 
 
+def map_to_many(seed_ranges: List[Tuple[int, int]], maps: List[str]) -> List[Tuple[int, int]]:
+    mapped = []
+    for row in maps:
+        ds, ss, r = list(map(int, row.split(' ')))
+        new = []
+        # print(f'{seeds=}')
+        while seed_ranges:
+            s, e = seed_ranges.pop()
+            b = (s, min(e, ss))
+            i = (max(s, ss), min(ds + r, e))
+            a = (max(ds + r, s), e)
+            if b[0] < b[1]:
+                # print(f'{s=}, {e=}, {row=}, adding {b} to new')
+                new.append(b)  
+            if i[0] > i[1]:
+                # print(f'{s=}, {e=}, {row=}, adding {(i[0] - ss + ds, i[1] - ss + ds)} to output of section')
+                mapped.append((i[0] - ss + ds, i[1] - ss + ds))
+            if a[0] < a[1]:
+                # print(f'{s=}, {e=}, {row=}, adding {a} to new')
+                new.append(a)
+        seed_ranges = new
+    return mapped + seed_ranges
+
+
 def solve_part_two(input_text: str) -> int:
     seedsies = [int(x) for x in input_text.split('\n\n')[0].replace('seeds: ', '').split(' ')]
     seeds = []
-    for i, s in enumerate(seedsies):
-        if i % 2 != 0:
-            seeds.append((seedsies[i - 1], s))
-    seed_map = {}
-    for seed_s, seed_r in seeds:
-        for s in range(seed_s, seed_s + seed_r):
-            source = s
-            d = source
-            for section in [x.splitlines()[1:] for x in input_text.split('\n\n')[1:]]:
-                for row in section:
-                    d_start, s_start, r = list(map(int, row.split(' ')))
+    for i in range(0, len(seedsies), 2):
+        seeds.append((seedsies[i], seedsies[i] + seedsies[i + 1]))
+    
+    sections = [x.splitlines()[1:] for x in input_text.split('\n\n')[1:]]
+    dests = []
+    for s, c in seeds:
+        start = [(s, s+c)]
+        for section in sections:
+            start = map_to_many(start, section)
+            print(f'{dests}')
+            dests.append(min(dests)[0])
 
-                    if s_start <= source < (s_start + r):
-                        if d_start > s_start:
-                            offset = d_start - s_start
-                            m = 1
-                        else:
-                            offset = s_start - d_start
-                            m = -1
-                        d = source + offset * m
-                source = d
-            seed_map[s] = d
-
-    total = min(seed_map.values())
-
-    return total
+    
+    return min(dests)
 
 
 if __name__ == '__main__':
